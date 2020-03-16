bd3332e1fd2bad3016cd7a4bf2567ba1d3b6aab3
dulcetiseduce <swinnaiscool@gmail.com>
2020-03-16 12:02:29 +0100

    aee cleanup

--- src/ThreadCostruttore.java
+++ src/ThreadCostruttore.java
@@ -1,8 +1,8 @@
 public class ThreadCostruttore extends Thread {
-    public void run(){
-        System.out.println("Ciao sono il Thread"+getName());
+    public void run() {
+        System.out.println("Ciao sono il Thread" + getName());
         int y = 30;
-        System.out.println("Ho letto la variabile y="+y);
+        System.out.println("Ho letto la variabile y=" + y);
     }
 
 }
