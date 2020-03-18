f5394988f03b70a8a7bd851589aab933abb11e01
Mirko7222 <20xgame02@gmail.com>
2020-03-18 10:24:09 +0100

    fatto

--- src/ContaThread.java
+++ src/ContaThread.java
@@ -6,6 +6,7 @@ public class ContaThread extends Thread {
         for (int i = 0; i < 10; i++) {
             System.out.println("Ciao" + i);
         }
+
     }
 
 }
--- src/ThreadCostruttore.java
+++ src/ThreadCostruttore.java
@@ -1,6 +1,7 @@
 public class ThreadCostruttore extends Thread {
     public void run() {
-        System.out.println("Ciao sono il Thread" + getName());
+        setName("Prova");
+        System.out.println("Ciao sono il Thread"+" "+ getName());
         int y = 30;
         System.out.println("Ho letto la variabile y=" + y);
     }
